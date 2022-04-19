29885a530012c97355c04f3e459caf13e1ff5b79
dr2moscow <dr2moscow@gmail.com>
2022-04-19 23:14:34 +0300

    Перевел проект на postgreSQL

--- Habr/settings.py
+++ Habr/settings.py
@@ -83,13 +83,15 @@ WSGI_APPLICATION = 'Habr.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
+    # 'sqlite3': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # },
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    },
-    'postgresql': {
+   #     'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'habr_dev',
+   #     'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
         'USER': 'postgres',
         'PASSWORD': 'admin',
         'HOST': 'localhost',
