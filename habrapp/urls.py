f43c4ac2908c8d006313a468e55c0fd7103d30dd
vvmokrenko <v.v.mokrenko@gmail.com>
2022-05-17 19:08:12 +0300

    Функционал "Создание хабра"
    1) Добавлен CKEditor для редактирования содержимого хабра
    2) Пофикшено получение slug для кириллицы

--- habrapp/urls.py
+++ habrapp/urls.py
@@ -1,9 +1,10 @@
-from django.urls import path
+from django.urls import path, include
 
 from .views import habr_create
 
 app_name = 'habrapp'
 
 urlpatterns = [
-             path('create/', habr_create, name='create'),
+    path('create/', habr_create, name='create'),
+    path('ckeditor/', include('ckeditor_uploader.urls')),
 ]
