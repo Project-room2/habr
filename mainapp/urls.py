d5f27dc85f49e457fd4e842418716fc7d38719a8
dr2moscow <dr2moscow@gmail.com>
2022-05-18 21:26:08 +0300

    Закрыл от индексации сайт - robots.txt

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -1,3 +1,5 @@
+from django.views.generic import TemplateView
+
 import mainapp.views as mainapp
 from django.urls import path
 from .views import *
@@ -9,4 +11,8 @@ urlpatterns = [
     path('habr/<slug:habr_slug>/', HabrView.as_view(), name = 'habr'),
     path('<slug:cat_slug>/', SectionView.as_view(), name = 'section'),
     path('like/<int:pk>/', mainapp.LikeView, name='like_post'),
+    path(
+        "robots.txt",
+        TemplateView.as_view(template_name = "robots.txt", content_type = "text/plain"),
+    ),
 ]
