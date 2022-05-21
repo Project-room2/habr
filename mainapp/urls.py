8022f7e89bd8d77fdde3524e179ba7a41deabe24
dr2moscow <dr2moscow@gmail.com>
2022-05-21 12:27:01 +0300

    упростил дизайн

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -1,12 +1,20 @@
+from django.views.generic import TemplateView
 import mainapp.views as mainapp
 from django.urls import path
+
+from . import views
 from .views import *
 
 app_name = 'mainapp'
 
 urlpatterns = [
-    path('', IndexView.as_view(), name='index'),
-    path('habr/<slug:habr_slug>/', HabrView.as_view(), name='habr'),
-    path('<slug:cat_slug>/', SectionView.as_view(), name='section'),
+    path('', IndexView.as_view(), name = 'index'),
+    path('habr/<slug:habr_slug>/', HabrView.as_view(), name = 'habr'),
+    path('<slug:cat_slug>/', SectionView.as_view(), name = 'section'),
     path('like/<int:pk>/', mainapp.LikeView, name='like_post'),
-    ]
+    path('profile/<int:pk>/', UserView.as_view(), name='profile'),
+    path(
+        "robots.txt",
+        TemplateView.as_view(template_name = "robots.txt", content_type = "text/plain"),
+    ),
+]
