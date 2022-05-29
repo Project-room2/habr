55eb7d0e1a1eb69a09b0b4e8abbcdff746580701
dr2moscow <dr2moscow@gmail.com>
2022-05-29 18:41:10 +0300

    изменения схемы хранения settigs

---
+++ Habr/settings/development.py
@@ -0,0 +1,21 @@
+from .base import *
+
+DEBUG = True
+
+ALLOWED_HOSTS = ['127.0.0.1', '127.0.0.1:8000']
+CSRF_TRUSTED_ORIGINS = ['http://127.0.0.1']
+
+INSTALLED_APPS = INSTALLED_APPS + ['debug_toolbar']
+MIDDLEWARE = ['debug_toolbar.middleware.DebugToolbarMiddleware'] + MIDDLEWARE
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join('db.sqlite3'),
+    }
+}
+
+
+ACCOUNT_EMAIL_VERIFICATION = 'none'
+EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
+EMAIL_FILE_PATH = 'tmp/email-messages/'
---
+++ Habr/settings/production.py
@@ -0,0 +1,45 @@
+from .base import *
+
+DEBUG = False
+
+ALLOWED_HOSTS = ['*.4t-habr.ru', '127.0.0.1', '127.0.0.1:8000']
+CSRF_TRUSTED_ORIGINS = ['https://4t-habr.ru']
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+
+SECURE_SSL_REDIRECT = True
+SECURE_SSL_HOST = "4t-habr.ru"
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+SECURE_BROWSER_XSS_FILTER = True
+
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
+ACCOUNT_EMAIL_VERIFICATION = 'none'
+DOMAIN_NAME = '4t-habr.ru'
+EMAIL_HOST = 'localhost'
+EMAIL_PORT = '25'
+EMAIL_SUBJECT_PREFIX = "[4t-habr] "
+# EMAIL_HOST_USER = 'i@4t-habr.ru'
+# EMAIL_HOST_PASSWORD = 'admin'
+EMAIL_USE_TLS = True
